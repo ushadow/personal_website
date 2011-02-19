4c807b938ba61947dc74537ec95639be0c857bf7
Ying Yin <yingyin@miscoe.(none)>
2011-02-18 23:31:28 -0500

    Initial commit of Ying Yin's personal website.

---
+++ script/utils.js
@@ -0,0 +1,16 @@
+function init()
+{
+	imgs = document.getElementsByTagName('img');
+	for(i=0;i<imgs.length;i++)
+	{
+		if(imgs[i].className=="favicon")
+		{		
+			url = imgs[i].parentNode.getAttribute('href');
+			var urlpattern = new RegExp("(http://(.*?)/).*$");
+			var parsedurl = url.match(urlpattern);
+			urlParts = url.split("/");
+			imgs[i].setAttribute('src', parsedurl[1]+'favicon.ico');
+		}
+	}
+}
+
