f64342867e4cd2d84b463b9964e1900e0bae1bba
yingyin <clevershadow@gmail.com>
2011-03-09 16:56:20 -0500

    Cleaned up links.

--- script/utils.js
+++ script/utils.js
@@ -1,15 +1,12 @@
-function init()
-{
+function init() {
 	imgs = document.getElementsByTagName('img');
-	for(i=0;i<imgs.length;i++)
-	{
-		if(imgs[i].className=="favicon")
-		{		
+	for(i=0; i < imgs.length; i++) {
+		if(imgs[i].className == "favicon") {		
 			url = imgs[i].parentNode.getAttribute('href');
 			var urlpattern = new RegExp("(http://(.*?)/).*$");
 			var parsedurl = url.match(urlpattern);
 			urlParts = url.split("/");
-			imgs[i].setAttribute('src', parsedurl[1]+'favicon.ico');
+			imgs[i].setAttribute('src', parsedurl[1] + 'favicon.ico');
 		}
 	}
 }
